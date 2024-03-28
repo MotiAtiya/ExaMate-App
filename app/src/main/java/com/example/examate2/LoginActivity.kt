f7ca14252c68bccd9a588d703d89fe8980dcdd40
MotiAtiya <a.moti96@gmail.com>
2024-03-28 19:27:58 +0200

    first commit

---
+++ app/src/main/java/com/example/examate2/LoginActivity.kt
@@ -0,0 +1,50 @@
+package com.example.examate
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.examate.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var binding : ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("LoginActivity","on Create")
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        val saveButton = binding.buttonSave
+        saveButton.setOnClickListener {
+            // Retrieve values from EditText fields
+
+            val firstName = binding.firstName.text.toString()
+            val lastName = binding.lastName.text.toString()
+            val email = binding.email.text.toString()
+
+// Validate input if needed
+            if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty()) {
+                // Display error message if any field is empty
+                Toast.makeText(applicationContext, R.string.fill_all_fields_message, Toast.LENGTH_SHORT).show()
+            } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                // Display error message if email format is invalid
+                Toast.makeText(applicationContext, R.string.invalid_email_message, Toast.LENGTH_SHORT).show()
+            } else {
+                // Process the form data if all validations pass
+                saveFormData(firstName, lastName, email)
+            }
+        }
+
+    }
+    // Method to save form data
+    private fun saveFormData(firstName: String, lastName: String, email: String) {
+        Toast.makeText(applicationContext, R.string.save_succeeded, Toast.LENGTH_SHORT).show()
+        val intent = Intent(this,HomeActivity::class.java).apply {}
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/example/examate2/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.examate
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.examate.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.login.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java).apply {}
+            startActivity(intent)
+        }
+    }
+}
